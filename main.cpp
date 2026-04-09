668a3fdccc100801ca844e74869d3c88a9742ddf
MinhHieuNotOkay <vuhieu26107@gmail.com>
2026-04-09 14:10:05 +0700

    main
    Initialize the database at the start of the application.

--- main.cpp
+++ main.cpp
@@ -1,11 +1,17 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include "DatabaseManager.h"
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
+    // Khởi tạo Database
+    if (!DatabaseManager::initDatabase()) {
+        return -1;
+    }
+
     MainWindow w;
     w.show();
-    return QCoreApplication::exec();
+    return a.exec();
 }
